3531d19e1a570c77d5413f223ea334b256475469
wendyhamel <whamel@it4web.nl>
2023-12-18 18:47:55 +0100

    Responsive classes

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,15 +1,17 @@
 tailwind.config = {
 	theme: {
-		fontFamily: {
-			sans: ['DM Sans', 'sans-serif']
-		},
-		colors: {
-			'red': 'hsl(10, 79%, 65%)',
-			'cyan': 'hsl(186, 34%, 60%)',
-			'dark-brown': 'hsl(25, 47%, 15%)',
-			'light-brown': 'hsl(28, 10%, 53%)',
-			'cream': 'hsl(27, 66%, 92%)',
-			'pale-orange': 'hsl(33, 100%, 98%)',
-		},
+		extend: {
+			fontFamily: {
+				sans: ['DM Sans', 'sans-serif']
+			},
+			colors: {
+				'red': 'hsl(10, 79%, 65%)',
+				'cyan': 'hsl(186, 34%, 60%)',
+				'dark-brown': 'hsl(25, 47%, 15%)',
+				'light-brown': 'hsl(28, 10%, 53%)',
+				'cream': 'hsl(27, 66%, 92%)',
+				'pale-orange': 'hsl(33, 100%, 98%)',
+			},
+		}
 	}
 }
